0f6332ddaea15e87c82e460bff0654550727f3a2
Flambard Alan <alanfla@hotmail.fr>
2016-09-14 19:33:53 +0200

    Correction de la page "A propos"
    J'ai fais toute les corrections de la page "A propos"

--- propos.php
+++ propos.php
@@ -19,41 +19,41 @@ require ("fonction.php");
 
     <h1 class="pré7">A propos de moi.</h1>
 
-    <p class="text1">Cette page vous permettera dans apprendre un peu plus de truc sur moi.</p>
+    <p class="text1">Cette page vous permettra dans apprendre un peu plus de truc sur moi.</p>
 
-    <p class="text1">Bonjour a toi je me nomme alan flambard, j'ai 15 ans et j'ai commencé youtube le 30 avril 2012.</p>
+    <p class="text1">Bonjour à toi je me nomme alan flambard, j'ai 15 ans et j'ai commencé YouTube le 30 avril 2012.</p>
 
     <p class="text1">J'ai atteint les 100 abonnées au bout d'environ quelque mois.</p>
 
-    <p class="text1">J'avais voulu faire un live pour célébrer cela mais sa ne c'est pas réaliser car je n'avais pas le débit internet nécessaire.</p>
+    <p class="text1">J'avais voulu faire un live pour célébrer cela mais ça ne s'est pas réaliser car je n'avais pas le débit internet nécessaire.</p>
 
-    <p class="text1">Après j'ai commencé a faire un launcher avec un modpack sur la version 1.4.7 de minecraft où qu'il y avait que 130 mods environ, ce qui beaucoup a l'époque.</p>
+    <p class="text1">Après j'ai commencé à faire un launcher avec un modpack sur la version 1.4.7 de minecraft où qu'il y avait que 130 mods environ, ce qui beaucoup à l'époque.</p>
 
     <p class="text1">Puis fut le temps de passé sur une nouvelle version de minecraft qui a étais la version 1.6.4 qui étais avec 140 mods environ.</p>
 
-    <p class="text1">Sur cette version là j'avait fais un launcher mais a un môment le launcher ne marcher plus alors j'ai dus trouvez une alternative a cela et j'ai un installeur que l'on peut personnaliser est c'est comme sa que la version 1.6.4 a pus continuer.</p>
+    <p class="text1">Sur cette version là j'avais fait un launcher mais a un moment le launcher ne marcher plus alors j'ai dus trouvez une alternative a cela et j'ai un installeur que l'on peut personnaliser est c'est comme ça que la version 1.6.4 a pu continuer.</p>
 
-    <p class="text1">Mais encore une fois j'ai changer de version, je suis passer a la version 1.7.10 et on est a 150 mods environ.</p>
+    <p class="text1">Mais encore une fois j'ai changé de version, je suis passé à la version 1.7.10 et on est à 150 mods environ.</p>
 
     <p class="text1">Et c'est la version actuel du modpack que vous pouvez télécharger <a href="assets/installer-ftb-modified-1.7.10.jar">ici</a>.</p>
 
-    <p class="text4">Et maintenant j'apprend a coder dans le language html, css, javascript et php tous en même temps.</p>
+    <p class="text4">Et maintenant j'apprends à coder dans le langage html, css, JavaScript et PHP tous en même temps.</p>
 
-    <p class="text2">Pour toute les question sur ce que j'utilise pour réaliser mes vidéo elle seront ci-dessous :</p>
+    <p class="text2">Pour toute les questions sur ce que j'utilise pour réaliser mes vidéos elles seront ci-dessous :</p>
 
-    <p class="text3">1. Pour filmer mon écran j'utilise soit camtasia pour filmer le bureau, soit fraps pour les jeux ou sinon pour l'exterieure j'utlise mon téléphone personnel <a href="http://fr.coolicool.com/expedies-de-france-cubot-x6-mtk6592-17ghz-octa-core-50-pouces-hd-ecran-android-42-3g-smartphone-g-192211" target="_blank">le cubot x6</a>.</p>
+    <p class="text3">1. Pour filmer mon écran j'utilise soit camtasia pour filmer le bureau, soit fraps pour les jeux ou sinon pour l'extérieure j'utilise mon téléphone personnel <a href="http://fr.coolicool.com/expedies-de-france-cubot-x6-mtk6592-17ghz-octa-core-50-pouces-hd-ecran-android-42-3g-smartphone-g-192211" target="_blank">le cubot x6</a>.</p>
 
     <p class="text3">2. Pour la partie audio j'utilise le <a href="http://www.thomann.de/fr/the_tbone_mb_95.htm" target="_blank">the tbone mb95</a>.</p>
 
-    <p class="text3">3. Pour monter mes vidéo j'utilise maintenant Adobe Première pro.</p>
+    <p class="text3">3. Pour monter mes vidéos j'utilise maintenant Adobe Première pro.</p>
 
-    <p class="text1">Bon voila je pense que vous avez toute les info, si vous avez besoin d'autre renseignement contacter moi ci-dessous.</p>
+    <p class="text1">Bon voilà je pense que vous avez toute les infos, si vous avez besoin d'autre renseignement contacter moi ci-dessous.</p>
 
     <iframe class="inframe" src="https://docs.google.com/forms/d/1A_FyPwVgs8Hdm_pbPn-vaaAzW-RIJdpaE711ibKgZdU/viewform?embedded=true" width="760" height="700" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
 
     <p class="text1">Merci d'avoir lu cette page vous pouvez me suivre sur <a href="http://www.youtube.com/user/alal700?feature=mhee" target="_blank">youtube</a>, aussi sur <a href="https://www.facebook.com/alan.flambard.5" target="_blank">facebook</a> et bien sûr <a href="https://twitter.com/ftbmodified" target="_blank">twitter</a>.</p>
 
-    <p class="mfooter">jbviupedb</p>
+    <p class="mfooter">a</p>
 
 
 <footer>
